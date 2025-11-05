5ee2c3f61984fb9c7b194746d0b47bf27fc4ff11
wang.xiang <wang.xiang@asiainfo-sec.com>
2025-11-05 16:04:44 +0800

    init

---
+++ model/src/main/java/com/xiang/order/bean/Order.java
@@ -0,0 +1,18 @@
+package com.xiang.order.bean;
+
+import com.xiang.product.bean.Product;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Data
+public class Order {
+    private Long id;
+    private BigDecimal totalAmount;
+    private Long userId;
+    private String nickName;
+    private String address;
+    private List<Product> productList;
+}
---
+++ services/service-order/src/main/java/com/xiang/order/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.xiang.order.service;
+
+
+public interface OrderService {
+
+    Order createOrder(Long productId,Long userId);
+}
