72e2a30103eff44ddff89ec5db62b92a3b1b0b7a
wang.xiang <wang.xiang@asiainfo-sec.com>
2025-11-12 17:58:01 +0800

    gateway

--- gateway/src/main/java/com/xiang/demo/filter/OnceTokenGatewayFilterFactory.java
+++ gateway/src/main/java/com/xiang/demo/filter/OnceTokenGatewayFilterFactory.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -19,6 +20,8 @@ public class OnceTokenGatewayFilterFactory extends AbstractNameValueGatewayFilte
         return new GatewayFilter() {
             @Override
             public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                //ServerHttpRequest request = exchange.getRequest();
+
                 //每次响应之前，添加一个一次性令牌，支持 uuid，jwt等各种格式
                 return chain.filter(exchange).then(Mono.fromRunnable(()->{
                     ServerHttpResponse response = exchange.getResponse();
